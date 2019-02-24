432ec0d94e9759eecb39642ff9895fa317d8cd21
Reid <Reid@pokersprout.com>
2019-02-23 21:01:57 -0500

    First commit
    First commit
    ———————————
    GraphQL schema and data type design.
    GraphQL query design & graphical testing.
    MongoDB connection with mLab.
    MongoDB schema definition and import.

---
+++ server/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+// Connect to mLab database
+const username = `Reid`;
+const password = `test123`;
+const options = { useNewUrlParser: true };
+
+mongoose.connect(`mongodb://${username}:${password}@ds249035.mlab.com:49035/gql-reid`, options);
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+
+// Middlewear
+app.use('/graphql', graphqlHTTP({
+    // Options for express-graphql
+    schema,
+    graphiql: true
+}));
+
+// Routing
+app.get('/', (req, res) => {
+    res.send(`<style>h1 {color:tomato;}</style><h1>Hello!</h1>`);
+});
+
+// Listen for server on given port
+app.listen(port, () => `Server started on port ${port}`);
